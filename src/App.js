641f33e506c8d2ecda1347bd38fa73380181144d
Mohamed abdelhaleam <mohamedabdelhaleam13@gmail.com>
2023-09-09 12:07:54 +0300

    Create Ui For Counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="max-w-[1280px] m-auto">
+      <div className='text-lg font-bold m-auto text-center my-16'>Counter : 0</div>
+      <div className='m-auto text-center'>
+        <button className=' bg-red-400 text-white rounded-md py-2 px-6 mx-4'>Increase</button>
+        <button className=' bg-red-400 text-white rounded-md py-2 px-6 mx-4'>Decrease</button>
+      </div>
     </div>
   );
 }
