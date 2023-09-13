0c79c6acb7da5786e1e1f096cc392cb153e4f52f
Mohamed abdelhaleam <mohamedabdelhaleam13@gmail.com>
2023-09-13 19:13:14 +0300

    Create Store File

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,22 +1,8 @@
-import {createStore} from 'redux'
-const initState = {value: 20, show : true}
-const counterReducer = (state = initState,action)=>{
-    switch (action.type) {
-        case ("increase"):
-            return {...state , value:state.value + 1}
-        case ("decrease"):
-            return { ...state ,value:state.value - 1 }
-        case ("increaseByFive"):
-            return { ...state ,value:state.value + action.payload  }
-        case ("toggle"):
-            return { ...state , show:!state.show  }
-        default:
-            
-    }
-    return state
-}
+import {configureStore} from '@reduxjs/toolkit'
+import counterReducer from './counterSlice'
 
 
-const store = createStore(counterReducer)
+const store = configureStore()
+
 
 export default store
