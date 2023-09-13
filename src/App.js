e411c7e5a0e814f0cf6f87fa2ec06b20c2f9976a
Mohamed abdelhaleam <mohamedabdelhaleam13@gmail.com>
2023-09-13 19:12:45 +0300

    Convert Redux to Redux Toolkit

--- src/App.js
+++ src/App.js
@@ -1,49 +1,28 @@
 import { useDispatch, useSelector } from 'react-redux';
+import { increase,decrease } from './store/counterSlice';
 import './App.css';
 
 function App() {
-  const dispatch = useDispatch()
-  const state = useSelector((state)=>{
-    if (state.value < 0) {
-      return  "No Number"
-    };
-    return state.value
-  })
-  const toggleState = useSelector((state)=>{
-    return state.show
-  })
 
-  const increase =()=>{
-    const action = {type:"increase"}
-    dispatch(action)
-  }
-  const increaseByFive =()=>{
-    const action = {type:"increaseByFive" , payload: 5}
-    dispatch(action)
-  }
-  const decrease =()=>{
-    const action = {type:"decrease"}
-    dispatch(action)
-  }
-  const toggleCounter = (e)=>{
-    const action = {type : "toggle"}
-    dispatch(action)
-  }
+  const globalState = useSelector((state)=> state)
+  const dispatch = useDispatch()
+
   return (
     <div className="max-w-[1280px] m-auto">
+      <div className='text-6xl font-bold m-auto text-center my-16'>Redux Basic </div>
       {
-        toggleState && <>
-            <div className='text-lg font-bold m-auto text-center my-16'>Counter : {state}</div>
+        <>
+            <div className='text-lg font-bold m-auto text-center my-16'>Counter : {globalState.value}</div>
             <div className='m-auto text-center'>
-              <button className=' bg-red-400 text-white rounded-md py-2 px-6 mx-4' onClick={increase}>Increase</button>
-              <button className=' bg-red-400 text-white rounded-md py-2 px-6 mx-4' onClick={increaseByFive}>Increase Five</button>
-              <button className=' bg-red-400 text-white rounded-md py-2 px-6 mx-4' onClick={decrease}>Decrease</button>
+              <button className=' bg-red-400 text-white rounded-md py-2 px-6 mx-4' onClick={()=> dispatch(increase(1))}>Increase</button>
+              <button className=' bg-red-400 text-white rounded-md py-2 px-6 mx-4' onClick={()=> dispatch(increase(5))}>Increase Five</button>
+              <button className=' bg-red-400 text-white rounded-md py-2 px-6 mx-4' onClick={()=> dispatch(decrease(1))}>Decrease</button>
             </div>
         </>
       }
       
       <div className='w-full m-auto text-center my-20'>
-        <button className=' bg-red-400 text-white m-auto text-center rounded-md py-2 px-6 mx-4' onClick={toggleCounter}>Show And Hide Counter</button>
+        <button className=' bg-red-400 text-white m-auto text-center rounded-md py-2 px-6 mx-4'>Show And Hide Counter</button>
       </div>
     </div>
   );
